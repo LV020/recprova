e53d4a009ced7beb789f0bed74bbee6afd9a6959
Aluno SENAI <Aluno@SN-363429.edu.fiesc.com.br>
2025-04-24 09:45:50 -0300

    Correção Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- recprova.js
+++ recprova.js
@@ -1,26 +1,27 @@
-//calsse base
+// Classe base
 class Personagem {
-    constructor(vida, mana, força){
+    constructor(vida, mana, força) {
         this.vida = vida;
         this.mana = mana;
         this.força = força;
     }
 }
-class Guerreiro extends Personagem {}
-    constructor(vida, mana, força)
-    super(vida, mana)
-    this.força = 20;
-    
-
-    class Mago extends Personagem {}
-    constructor(vida, força, mana)
-    super(vida, força)
-    this.mana = 150;
 
 
-    class Arqueiro extends Personagem {}
-    constructor(mana, força, vida)
-    super(mana, força)
-    this.vida = 130;
+class Guerreiro extends Personagem {
+    constructor(vida, mana) {
+        super(vida, mana, 20);
+    }
+}
 
-    
+class Mago extends Personagem {
+    constructor(vida, força) {
+        super(vida, 150, força);
+    }
+}
+
+class Arqueiro extends Personagem {
+    constructor(mana, força) {
+        super(130, mana, força);
+    }
+}
